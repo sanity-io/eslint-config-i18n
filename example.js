b87be0ea080723f028b687589de4ad692eb7eea1
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-01-22 15:48:00 -0800

    feat: enable no-i18next-import rule

--- example.js
+++ example.js
@@ -1,8 +1,11 @@
+import {useTranslation} from 'react-i18next';
+
 function Component() {
+  const { t } = useTranslation();
   return (
     <>
       string literal{' '}
-      <Button stringAttr="hello" templateAttr={`hello ${name}`} />
+      <Button stringAttr="hello" templateAttr={`hello ${name}`} aria-label={t('some.key')} />
     </>
   );
 }
